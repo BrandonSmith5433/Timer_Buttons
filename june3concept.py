451da4842318067b1cedb0e3a88e78f59342cf83
brandonsmith5433 <brandonsmith5433@gmail.com>
2024-06-03 19:16:29 -0700

    a

--- june3concept.py
+++ june3concept.py
@@ -1,6 +1,7 @@
 from gpiozero import Button, LED
-from signal import pause
 from time import sleep, time
+from signal import pause
+import csv
 
 whiteButton = Button(27, bounce_time=.1, hold_time = 1)
 whiteLED = LED(17)
@@ -21,36 +22,57 @@ ButtonList = [greenButton, redButton, yellowButton, blueButton]
 timeList = [0,0,0,0]
 LEDList = [greenLED, redLED, yellowLED, blueLED]
 
-start = 0
-end = 0
-pauseStart = 0
-pauseEnd = 0
-y = 0
+#Global Variables
+held = 0
 z = 0
+start = float(0)
+end = float(0)
+pauseStart = float(0)
+pauseEnd = float(0)
 
-while y < 7:
+
+#Startup Sequence
+y = 0
+while y < 5:
     for x in LEDList:
         x.on()
         sleep(.2)
         x.off()
-    y = y + 1     
+    y = y + 1
 
-def press_button():
-    global held
-    global z
+#Will turn on your light for your turn
+#Starts a timer
+def turn(z):
+    global start
+    LEDList[z].on()
+    start = time()
+    #holding the button will flash the lights and start a pause timer to be removed from the main timer
+    ButtonList[z].when_held = held_button
+    #just pressing the button will calculate the time and add it to your color, It will then reset timers
+    ButtonList[z].when_released = release_button
+    if z < 3:
+        z = z + 1
+        turn(z)
+    else:
+        z = 0
+        turn(z)
+
+def release_button():
+    global held, z, pauseStart, pauseEnd, start, end
     if held == 0:
         end = time()
         timeList[z] = timeList[z] + (end - start)
         timeList[z] = timeList[z] - (pauseEnd - pauseStart)
-        reset_timers()
+        pauseStart = 0
+        end = 0
+        start = 0
+        pauseEnd =0
         LEDList[z].off()
-        next_turn()
     else:
         held = 0
 
 def held_button():
-    global held
-    global z
+    global held, z, pauseStart, pauseEnd
     held = 1
     pauseStart = time()
     ButtonList[z].wait_for_press()
@@ -65,23 +87,15 @@ def next_turn():
         z = 0
         turn()
 
-def turn():
-    global z
-    while z != 4:
-        LEDList[z].on()
-        start = time()
-        ButtonList[z].wait_for_press()
-        ButtonList[z].when_held = held_button
-        ButtonList[z].when_released = press_button
-        
-def reset_timers():
-    global start, end, pauseStart, pauseEnd
-    start = 0
-    end = 0
-    pauseStart = 0
-    pauseEnd = 0
-
+#writes data to CSV at end
 def output():
-    pass
+    with open('output.csv', 'w', newline='') as csvfile: 
+        writer = csv.writer(csvfile) 
+        writer.writerow(['Player Color', 'Total Time'])
+        for i in range(len(ButtonList)): 
+            writer.writerow([ButtonList[i], timeList[i]])
 
+#waits for white to be pressed     
+ButtonList[4].when_pressed = output()
+#the start of it all
 turn()
